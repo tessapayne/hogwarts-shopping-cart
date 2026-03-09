fa0385d6b50373fee0809487053ab76bca8fec49
Tessa Payne <tessapayne@Tessas-MacBook-Pro.local>
2026-03-09 17:53:00 -0500

    Episode 17: watchers hogwarts-shopping-cart

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,93 @@
 <template>
   <div class="shopping-cart">
-    <h1>Harry's Shopping Cart</h1>
+    <h1>{{ username }}'s Shopping Cart</h1>
     <div class="cart-container">
       <div class="cart-list">
-  <div
-    class="cart-list-item"
-    v-for="item in shoppingCartItems"
-    :key="item.id"
-  >
-    <img :src="item.image" :alt="item.productName" class="product-image">
-
-    <div class="item-details-with-actions">
-      <div class="item-details">
-        <h2>{{ item.productName }}</h2>
-        <p class="price">${{ item.price }}</p>
-
-        <p v-if="item.isInStock" class="in-stock-status">
-          <i class="fa-solid fa-check"></i> In stock
-        </p>
-
-        <p v-else class="on-backorder-status">
-          <i class="fa-solid fa-hourglass-half"></i> On backorder
-        </p>
-      </div>
-
-      <div class="item-actions">
-        <div class="quantity-selector">
-          <button class="quantity-change-button" @click="decreaseOne(item.id)">−</button>
-
-          <input
-            type="text"
-            class="quantity-input"
-            :value="item.quantity"
-            aria-label="quantity"
-            readonly
-          >
-
-          <button class="quantity-change-button" @click="increaseOne(item.id)">+</button>
+        <div
+          class="cart-list-item"
+          v-for="item in shoppingCartItems"
+          :key="item.id"
+        >
+          <img
+            :src="item.image"
+            :alt="item.productName"
+            class="product-image"
+          />
+          <div class="item-details-with-actions">
+            <div class="item-details">
+              <h2>{{ item.productName }}</h2>
+              <p class="price">${{ item.price }}</p>
+              <p class="in-stock-status" v-if="item.isInStock">
+                <i class="fa-solid fa-check"></i> In stock
+              </p>
+              <p class="on-backorder-status" v-else>
+                <i class="fa-solid fa-hourglass-half"></i> On backorder
+              </p>
+            </div>
+            <div class="item-actions">
+              <div class="quantity-selector">
+                <button
+                  class="quantity-change-button"
+                  @click="decreaseOne(item.id)"
+                >
+                  −
+                </button>
+                <input
+                  type="text"
+                  class="quantity-input"
+                  v-model.number="item.quantity"
+                  aria-label="quantity"
+                />
+                <button
+                  class="quantity-change-button"
+                  @click="increaseOne(item.id)"
+                >
+                  +
+                </button>
+              </div>
+              <button class="remove-item" @click="removeItem(item.id)">
+                ✕
+              </button>
+            </div>
+          </div>
         </div>
-
-        <button class="remove-item" @click="removeItem(item.id)">✕</button>
       </div>
-    </div>
-  </div>
-</div>
-
       <div class="order-summary">
         <h2>Order summary</h2>
-
         <button
           class="toggle-details-button"
           @click="hideDetails = !hideDetails"
         >
           {{ hideDetails ? 'Show Details' : 'Hide Details' }}
         </button>
-
         <div :class="{ 'hide-order-details': hideDetails }">
           <div class="summary-item">
             <span>Subtotal</span>
             <span>${{ subtotal }}</span>
           </div>
-
           <div class="summary-item">
             <span>Shipping estimate</span>
             <span>${{ shippingEstimate }}</span>
           </div>
-
           <div class="summary-item">
             <span>Tax estimate</span>
             <span>${{ taxEstimate }}</span>
           </div>
         </div>
-
         <div class="summary-total">
           <strong>Order total</strong>
           <strong>${{ total }}</strong>
         </div>
-
         <button class="checkout-button">Checkout</button>
       </div>
     </div>
   </div>
 </template>
 
-  <script setup>
-   import { ref, computed } from 'vue'
+<script setup>
+import { computed, ref, watch } from 'vue'
 
+let username = 'Harry'
 let shoppingCartItems = ref([
   {
     id: 1,
@@ -101,7 +103,7 @@ let shoppingCartItems = ref([
     price: 600,
     isInStock: true,
     quantity: 2,
-    image: 'src/assets/img/GoldenSnitch.png'
+    image: ' src/assets/img/GoldenSnitch.png'
   },
   {
     id: 3,
@@ -131,212 +133,229 @@ let shoppingCartItems = ref([
 
 let hideDetails = ref(false)
 
+//decreases the quantity of an item when the minus button is clicked
 function decreaseOne(id) {
   shoppingCartItems.value.some((item) => {
-    if (item.id == id && item.quantity > 0) {
+    if (item.id == id && item.quantity != 0) {
       item.quantity = item.quantity - 1
-      return true
     }
-    return false
   })
 }
 
+//increases the quantity when the plus button is clicked
 function increaseOne(id) {
   shoppingCartItems.value.some((item) => {
     if (item.id == id) {
       item.quantity = item.quantity + 1
-      return true
     }
-    return false
   })
 }
 
+//removes an item from the shopping cart 
 function removeItem(id) {
-  let index = shoppingCartItems.value.findIndex((item) => item.id == id)
-  if (index !== -1) {
-    shoppingCartItems.value.splice(index, 1)
-  }
+  let index = shoppingCartItems.value.findIndex((item) => {
+    return item.id == id
+  })
+  shoppingCartItems.value.splice(index, 1)
 }
 
+//calculating subtotal --> adding price * quantity for each item
 let subtotal = computed(() =>
-  shoppingCartItems.value.reduce((acc, item) => acc + item.price * item.quantity, 0)
+  shoppingCartItems.value.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  )
 )
 
 let shippingEstimate = computed(() => (subtotal.value > 10000 ? 100 : 50))
 
 let taxEstimate = computed(() => subtotal.value * 0.08)
 
-let total = computed(() => subtotal.value + shippingEstimate.value + taxEstimate.value)
+let total = computed(
+  () => subtotal.value + shippingEstimate.value + taxEstimate.value
+)
+
+//the watcher
+//automatically saves the cart to localStorage whenever it changes
+watch(
+  shoppingCartItems,
+  () => {
+    localStorage.setItem(
+      'hogwartsShoppingCart',
+      JSON.stringify(shoppingCartItems.value)
+    )
+  },
+  { deep: true }
+)
 </script>
 
 <style scoped>
-        .shopping-cart {
-            font-family: 'Arial', sans-serif;
-            background-color: #f8f8f8;
-            margin: 0;
-            padding: 0;
-        }
+.shopping-cart {
+  font-family: 'Arial', sans-serif;
+  background-color: #f8f8f8;
+  margin: 0;
+  padding: 0;
+}
 
-        h1 {
-            padding: 20px;
-            max-width: 1200px;
-            margin: auto;
-        }
+h1 {
+  padding: 20px;
+  max-width: 1200px;
+  margin: auto;
+}
 
-        .cart-container {
-            display: flex;
-            align-items: flex-start;
-            max-width: 1200px;
-            margin: auto;
-        }
+.cart-container {
+  display: flex;
+  align-items: flex-start;
+  max-width: 1200px;
+  margin: auto;
+}
 
-        .cart-list {
-            flex-grow: 2;
-            margin-right: 20px;
-        }
+.cart-list {
+  flex-grow: 2;
+  margin-right: 20px;
+}
 
-        .order-summary {
-            flex-basis: 300px;
-            background-color: #f9fafb;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-        }
+.order-summary {
+  flex-basis: 300px;
+  background-color: #f9fafb;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
 
-        /* Styles for the cart list item */
-        .cart-list-item {
-            display: flex;
-            align-items: center;
-            background-color: #fff;
-            margin-bottom: 10px;
-            border-radius: 8px;
-            padding: 10px;
-        }
+.cart-list-item {
+  display: flex;
+  align-items: center;
+  background-color: #fff;
+  margin-bottom: 10px;
+  border-radius: 8px;
+  padding: 10px;
+}
 
-        .product-image {
-            width: 100px;
-            margin-right: 10px;
-        }
+.product-image {
+  width: 100px;
+  margin-right: 10px;
+}
 
-        .item-details-with-actions {
-            display: flex;
-            flex-grow: 1;
-            margin-left: 10px;
-        }
+.item-details-with-actions {
+  display: flex;
+  flex-grow: 1;
+  margin-left: 10px;
+}
 
-        .item-details {
-            flex-grow: 1;
-        }
+.item-details {
+  flex-grow: 1;
+}
 
-        .item-actions {
-            display: flex;
-            align-items: center;
-        }
+.item-actions {
+  display: flex;
+  align-items: center;
+}
 
-        .price {
-            font-size: 0.9em;
-            color: #666;
-        }
+.price {
+  font-size: 0.9em;
+  color: #666;
+}
 
-        .in-stock-status {
-            font-size: 0.9em;
-            color: green;
-        }
+.in-stock-status {
+  font-size: 0.9em;
+  color: green;
+}
 
-        .on-backorder-status {
-            font-size: 0.9em;
-            color: red;
-        }
+.on-backorder-status {
+  font-size: 0.9em;
+  color: red;
+}
 
-        .quantity-selector {
-            display: flex;
-            border: 1px solid #c1c1c1;
-            border-radius: 8px;
-            overflow: hidden;
-        }
+.quantity-selector {
+  display: flex;
+  border: 1px solid #c1c1c1;
+  border-radius: 8px;
+  overflow: hidden;
+}
 
-        .quantity-change-button {
-            background-color: #ffffff;
-            border: none;
-            padding: 10px 12px;
-            cursor: pointer;
-            font-size: 1rem;
-            transition: all 0.2s;
-        }
+.quantity-change-button {
+  background-color: #ffffff;
+  border: none;
+  padding: 10px 12px;
+  cursor: pointer;
+  font-size: 1rem;
+  transition: all 0.2s;
+}
 
-        .quantity-input {
-            border: none;
-            text-align: center;
-            width: 40px;
-            padding: 10px;
-            font-size: 1rem;
-            transition: all 0.2s;
-        }
+.quantity-input {
+  border: none;
+  text-align: center;
+  width: 40px;
+  padding: 10px;
+  font-size: 1rem;
+  transition: all 0.2s;
+}
 
-        .quantity-input:focus {
-            outline: none;
-        }
+.quantity-input:focus {
+  outline: none;
+}
 
-        .remove-item {
-            background: none;
-            border: none;
-            border-radius: 50%;
-            cursor: pointer;
-            font-size: 1.2em;
-            margin-left: 20px;
-        }
+.remove-item {
+  background: none;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+  font-size: 1.2em;
+  margin-left: 20px;
+}
 
-        .quantity-change-button:hover,
-        .quantity-change-button:focus,
-        .quantity-input:focus,
-        .remove-item:hover,
-        .remove-item:focus {
-            background-color: #f2f2f2;
-        }
+.quantity-change-button:hover,
+.quantity-change-button:focus,
+.quantity-input:focus,
+.remove-item:hover,
+.remove-item:focus {
+  background-color: #f2f2f2;
+}
 
-        .toggle-details-button {
-            background-color: #f1f1f1;
-            color: #333;
-            padding: 10px 15px;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-            margin-bottom: 10px;
-        }
+.toggle-details-button {
+  background-color: #f1f1f1;
+  color: #333;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-bottom: 10px;
+}
 
-        .hide-order-details {
-            display: none;
-        }
+.hide-order-details {
+  display: none;
+}
 
-        .summary-item {
-            display: flex;
-            justify-content: space-between;
-            padding: 10px 0;
-        }
+.summary-item {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 0;
+}
 
-        .summary-total {
-            display: flex;
-            justify-content: space-between;
-            font-weight: bold;
-            padding: 10px 0;
-            border-top: 1px solid #e2e2e2;
-            margin-top: 10px;
-        }
+.summary-total {
+  display: flex;
+  justify-content: space-between;
+  font-weight: bold;
+  padding: 10px 0;
+  border-top: 1px solid #e2e2e2;
+  margin-top: 10px;
+}
 
-        .checkout-button {
-            background-color: #4F46E5;
-            color: #fff;
-            border: none;
-            padding: 10px 20px;
-            border-radius: 5px;
-            width: 100%;
-            cursor: pointer;
-            margin-top: 10px;
-            transition: all 0.2s;
-        }
+.checkout-button {
+  background-color: #4f46e5;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  width: 100%;
+  cursor: pointer;
+  margin-top: 10px;
+  transition: all 0.2s;
+}
 
-        .checkout-button:hover {
-            background-color: #4138D9;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-        }
+.checkout-button:hover {
+  background-color: #4138d9;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+}
 </style>
